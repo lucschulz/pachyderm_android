dee53d0947a4f5a8cd72e7a3fefe12034615c31d
Luc Schulz <luc@lucschulz.com>
2018-09-23 17:04:32 -0400

    no message

--- app/src/main/java/ca/lucschulz/pachyderm/MainActivity.java
+++ app/src/main/java/ca/lucschulz/pachyderm/MainActivity.java
@@ -3,7 +3,19 @@ package ca.lucschulz.pachyderm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private List<Book> bookList = new ArrayList<>();
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
