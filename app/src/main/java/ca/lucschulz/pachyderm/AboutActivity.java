e206d790f9291ecb7b7461565ffed914102cb6db
Luc Schulz <luc@lucschulz.com>
2018-11-18 17:53:26 -0500

    Minor tweaks. Published beta to Google Play Store.

--- app/src/main/java/ca/lucschulz/pachyderm/AboutActivity.java
+++ app/src/main/java/ca/lucschulz/pachyderm/AboutActivity.java
@@ -15,6 +15,11 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
+        TextView tvHeader = findViewById(R.id.tvAppTitleVersion);
+        String appTitle = getString(R.string.app_name);
+        String appVersion = getString(R.string.app_versionNumber);
+        tvHeader.setText(appTitle + " " + appVersion);
+
         configureLink();
     }
 
@@ -24,7 +29,7 @@ public class AboutActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.lucschulz.ca/"));
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(String.valueOf(R.string.app_homepage)));
                 startActivity(browserIntent);
             }
         });
